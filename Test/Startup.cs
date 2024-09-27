dc92afae03ffc6f7d6eea61386b8781f8df6fbb9
Vitor-Fazoli <vhfazoli@gmail.com>
2024-09-27 07:37:21 -0300

    Not organized, but with middleware 'app.UseDocsnap' working

---
+++ Test/Startup.cs
@@ -0,0 +1,24 @@
+using docsnap;
+
+namespace docsnapAPI;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configura a aplicação para usar a funcionalidade da biblioteca docsnapLib
+        app.UseDocsnap();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
